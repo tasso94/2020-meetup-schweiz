81774688c78f4ab09bc18c172c158c9532e81d87
Tassilo Weidner <tassilo.weidner@camunda.com>
2020-03-03 17:44:22 +0100

    Add GO Client full solution

--- assets/openapi-client-modify.go
+++ assets/openapi-client-modify.go
@@ -13,11 +13,29 @@ func main() {
 
 	var payload = openapi.ModifyProcessInstanceOpts{
 		ProcessInstanceModificationDto: optional.NewInterface(openapi.ProcessInstanceModificationDto{
-			Instructions: []openapi.ProcessInstanceModificationInstructionDto{},
+			Instructions: []openapi.ProcessInstanceModificationInstructionDto{
+				{
+					ActivityId: "zahl-raten-schwer",
+					Type:       "cancel",
+				},
+				{
+					ActivityId: "tipp-geben",
+					Type:       "startBeforeActivity",
+				},
+			},
 		}),
 	}
 
 	var ctx = context.Background()
 	var processInstanceId = os.Args[1]
 
+	var _, err = client.ProcessInstanceApi.ModifyProcessInstance(ctx, processInstanceId, &payload)
+
+	if err != nil {
+		panic(err)
+
+	} else {
+		println("Modifikation Erfolgreich!")
+
+	}
 }
